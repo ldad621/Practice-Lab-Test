3f13f8e5576868ad20ed4a3324432a4723fc733e
ldad621 <ldad621@aucklanduni.ac.nz>
2017-06-01 12:57:47 +1200

    prac1
    files still not working 1/06/17

---
+++ MovieScript.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+class MovieScript 
+{ 
+	public string MovieID { get; set; }
+	public string MovieTItle { get; set; }
+	public string ReleaseDate { get; set; }
+	public string IMBDURL { get; set; }
+}
